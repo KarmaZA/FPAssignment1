a3c665c49e215e94a579db56f0764edba21ce0c7
KarmaZA <everatt.jonathon@gmail.com>
2021-04-06 21:06:18 +0200

    Working on q4

--- partB-question4.hs
+++ partB-question4.hs
@@ -1,5 +1,5 @@
 split :: [Int] -> [([Int],[Int])]
 split [] = [([],[])]
-split [x] = [([x],[])]
+split [x] = error "Not enough elements"
 --So this isn't the correct syntax but I do think this is the route to the answer. use a generator from 1 to length and take that num from the list
-split xs = 
+split m =
