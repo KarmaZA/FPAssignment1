64e74ce2ffe21d749dec1ade0e86366ceeb70547
KarmaZA <everatt.jonathon@gmail.com>
2021-04-08 09:40:06 +0200

    need to restart the vm because it will crash otherwise

--- partB-question4.hs
+++ partB-question4.hs
@@ -3,5 +3,13 @@ split [] = [([],[])]
 split [x] = error "Not enough elements"
 --So this isn't the correct syntax but I do think this is the route to the answer. use a generator from 1 to length and take that num from the list
 split m = do
-	let l = length m
-	[m splitAt x | x <- [1..l]]
+        let l = length m
+        split 1 (l-1) xs
+
+split1 1 xs = do
+        let l = length xs
+	[(take l-1 xs),(drop l-1 xs)] 
+
+split1 x xs
+	let l = length xs
+	[(take l-x xs),(take l-x xs)] ++ split1 x xs
