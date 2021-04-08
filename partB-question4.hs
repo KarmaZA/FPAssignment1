686e633d67299a573ca816a59ba474ff9f96008d
KarmaZA <everatt.jonathon@gmail.com>
2021-04-08 12:27:21 +0200

    After many, many errors. I got it to work. haskel is hard :'(

--- partB-question4.hs
+++ partB-question4.hs
@@ -3,13 +3,13 @@ split [] = [([],[])]
 split [x] = error "Not enough elements"
 --So this isn't the correct syntax but I do think this is the route to the answer. use a generator from 1 to length and take that num from the list
 split m = do
-        let l = length m
-        split 1 (l-1) xs
+    let l = length m
+    split1 (l-1) m
 
 split1 1 xs = do
-        let l = length xs
-	[(take l-1 xs),(drop l-1 xs)] 
+    let l = length xs
+    [ ( take (l-1) xs, drop (l-1) xs) ] 
 
-split1 x xs
-	let l = length xs
-	[(take l-x xs),(take l-x xs)] ++ split1 x xs
+split1 x xs = do
+    let l = length xs
+    [ (take (l-x) xs, drop (l-x) xs) ] ++ split1 (x-1) xs
