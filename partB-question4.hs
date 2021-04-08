713965491530b665ecc1bf8620adc94f66831931
KarmaZA <everatt.jonathon@gmail.com>
2021-04-08 09:34:38 +0200

    trying to get B4 to work. Committing before I try adding functions to make it work so that I can roll back to this point if the functions is the wrong route to take

--- partB-question4.hs
+++ partB-question4.hs
@@ -2,4 +2,6 @@ split :: [Int] -> [([Int],[Int])]
 split [] = [([],[])]
 split [x] = error "Not enough elements"
 --So this isn't the correct syntax but I do think this is the route to the answer. use a generator from 1 to length and take that num from the list
-split m =
+split m = do
+	let l = length m
+	[m splitAt x | x <- [1..l]]
